07de1fdca71377dbce46f7267be04792b32b073b
KavehJahangiri <jahangirikaveh1@gmail.com>
2023-12-12 15:58:17 +0330

    initialize

---
+++ src/cfehome/utils.py
@@ -0,0 +1,20 @@
+from faker import Faker
+
+def get_fake_profiles(count=2):
+    fake = Faker()
+    user_data = []
+    for _ in range(count):
+        profile = fake.profile()
+        data = {
+            "username": profile.get('username'),
+            'email': profile.get('mail'),
+            'is_active': True
+        }
+        if 'name' in profile:
+            fname, lname = profile.get('name').split(' ')[:2]
+            data['first_name'] = fname
+            data['last_name'] = lname
+        user_data.append(data)
+    return user_data
+print(get_fake_profiles()) 
+
