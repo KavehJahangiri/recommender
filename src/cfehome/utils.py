cc3332ccfaafeb6a92a7af66ad209935052d6c82
KavehJahangiri <jahangirikaveh1@gmail.com>
2023-12-12 16:53:01 +0330

    added 46000 movies to the database
    added 46000 movies to the database

--- src/cfehome/utils.py
+++ src/cfehome/utils.py
@@ -1,4 +1,39 @@
+import csv
 from faker import Faker
+from pprint import pprint
+from django.conf import settings
+import datetime
+MOVIE_METADATA_CSV = settings.DATA_DIR / 'movies_metadata.csv'
+
+def validate_date_str(date_text):
+    try: 
+        datetime.datetime.strptime(date_text, "%Y-%m-%d")
+    except:
+        return None
+    return date_text
+
+def load_movie_data(limit=1):
+    with open(MOVIE_METADATA_CSV,  newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        dataset = []
+        for i, row in enumerate(reader):
+            _id = row.get('id')
+            try: 
+                _id = int(_id)
+            except:
+                _id = None
+            release_date = validate_date_str(row.get('release_date'))
+            data = {
+                "id": _id,
+                "title": row.get('title'),
+                "overview": row.get('overview'), 
+                'release_date': release_date
+            }
+            dataset.append(data)
+            if i + 1 > limit:
+                break
+        return dataset
+
 
 def get_fake_profiles(count=2):
     fake = Faker()
@@ -16,5 +51,4 @@ def get_fake_profiles(count=2):
             data['last_name'] = lname
         user_data.append(data)
     return user_data
-print(get_fake_profiles()) 
 
